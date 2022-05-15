b896034d5fca3e2d61d55dd3c4b5308b6c4d049d
wushi <>
2022-05-15 21:00:30 +0800

    添加可下载文件检测功能

--- trust/views.py
+++ trust/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, HttpResponse
 from trust.models import History
 from trust.detect_func import wjc
+import trust.detect as detect
 
 
 test_url = ''
 trust_value = 0
 contained_wjc = ''
+have_dangerous_file = False
 
 
 def suffix_detection():
@@ -18,7 +20,7 @@ def suffix_detection():
 
 def index(request: HttpResponse):
     if request.method == 'POST':
-        global test_url, trust_value, contained_wjc
+        global test_url, trust_value, contained_wjc, have_dangerous_file
         test_url = request.POST.get("url", None)
         wjc.wjc_detect(test_url)
         with open('OK.csv', 'r') as fp:
@@ -28,12 +30,13 @@ def index(request: HttpResponse):
         with open("OK.csv", 'r+') as f:
             f.truncate(0)
 
+        have_dangerous_file = detect.file_detect(test_url)
         trust_value = suffix_detection()
-        history = History(url=test_url, trust_value=trust_value)
+        history = History(url=test_url, trust_value=trust_value, have_dangerous_file=have_dangerous_file)
         history.save()
     return render(request,
                   'trust/index.html',
-                  {"test_url": test_url, "trust_value": trust_value, "contained_wjc": contained_wjc, }
+                  {"test_url": test_url, "trust_value": trust_value, "contained_wjc": contained_wjc,  "have_dangerous_file": have_dangerous_file}
                   )
 
 
